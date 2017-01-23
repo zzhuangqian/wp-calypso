d18c9b238fba2fdfd7c47fdeb7c2c46eba5c8dbd
Derek Smart <smart@automattic.com>
2017-01-23 11:19:00 -1000

    style activity log date

--- client/my-sites/stats/activity-log/date.jsx
+++ client/my-sites/stats/activity-log/date.jsx
@@ -7,19 +7,26 @@ import { localize } from 'i18n-calypso';
  * Internal dependencies
  */
 import FoldableCard from 'components/foldable-card';
+import Button from 'components/button';
 import ActivityLogItem from '../activity-log/item';
 
 const ActivityLogDate = React.createClass( {
 
     render() {
         return (
-            <FoldableCard
-                header={ 'Jan 1, 1999' }
-            >
-                <ActivityLogItem />
-                <ActivityLogItem />
-                <ActivityLogItem />
-            </FoldableCard>
+            <div className="activity-log-date">
+                <FoldableCard
+                    header={ <div><div>Jan, 01, 1999</div><div><small> 17 Events</small></div></div> }
+                    headerExpanded={ <div><div>Jan, 01, 1999</div><div><small> 17 Events</small></div></div> }
+                    summary={ <Button className="button">Rewind</Button> }
+                    expandedSummary={ <Button className="button">Rewind</Button> }
+                    clickableHeader={ true }
+                >
+                    <ActivityLogItem />
+                    <ActivityLogItem />
+                    <ActivityLogItem />
+                </FoldableCard>
+            </div>
         );
     }
 } );
