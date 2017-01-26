f4a97ab42a8775dcd625eeb51204f9ad889a5daa
Filipe Varela <keoshi@keoshi.com>
2017-01-26 13:49:01 -1000

    Update activity banner

--- client/my-sites/stats/activity-log-banner/index.jsx
+++ client/my-sites/stats/activity-log-banner/index.jsx
@@ -20,22 +20,22 @@ class ActivityLogBanner extends Component {
     getContent() {
         return (
             <div className="activity-log-banner__content">
-                <h2 className="activity-log-banner__content-title">This item requires your imediate attention</h2>
-                <div className="activity-log-banner__content-body">Jetpack found a treat in your Yoast SEO Plugin</div>
-                <div className="activity-log-banner__content-meta">17 January 2017 at 3:30 PM - Melware code detected</div>
+                <h2 className="activity-log-banner__content-title">This item requires your immediate attention</h2>
+                <div className="activity-log-banner__content-body">Jetpack found a threat in your Yoast SEO Plugin</div>
+                <div className="activity-log-banner__content-meta">17 January 2017 at 3:30 PM - Malware code detected</div>
             </div>
         );
     }
 
     getAction() {
-        return ( <Button primary>Fix Treat</Button> );
+        return ( <Button primary>Fix Threat</Button> );
     }
 
     render() {
         return (
             <Card className="activity-log-banner">
                 <div className="activity-log-banner__icon">
-                    <Gridicon icon="notice-outline" size={ 24 } />
+                    <Gridicon icon="notice" size={ 24 } />
                 </div>
                 { this.getContent() }
                 { this.getAction() }
