daaef11f7a3b429578204a377b3c4b346dc2d71b
Jake F - @samouri <samouri@users.noreply.github.com>
2017-02-14 16:13:48 -0500

    remove unused parts of action.js

--- client/state/reader/tags/items/actions.js
+++ client/state/reader/tags/items/actions.js
@@ -7,14 +7,8 @@ import { kebabCase } from 'lodash';
  * Internal dependencies
  */
 import {
-	READER_FETCH_TAG_REQUEST,
-	READER_FETCH_TAG_RECEIVE,
-	READER_FETCH_TAGS_REQUEST,
-	READER_FETCH_TAGS_RECEIVE,
 	READER_FOLLOW_TAG_REQUEST,
 	READER_FOLLOW_TAG_RECEIVE,
-	READER_UNFOLLOW_TAG_REQUEST,
-	READER_UNFOLLOW_TAG_RECEIVE,
 } from 'state/action-types';
 
 /**
@@ -24,37 +18,6 @@ import {
  * @return {String}      Tag slug
  */
 const slugify = ( tag ) => encodeURIComponent( kebabCase( tag ) );
-
-export const requestTags = () => ( { type: READER_FETCH_TAGS_REQUEST } );
-export const receiveTags = () => ( { type: READER_FETCH_TAGS_RECEIVE } );
-
-export const requestTag = ( tag ) => ( {
-	type: READER_FETCH_TAG_REQUEST,
-	payload: {
-		tag,
-		slug: slugify( tag ),
-	},
-} );
-
-export const receiveTag = ( { payload, error } ) => ( {
-	type: READER_FETCH_TAG_RECEIVE,
-	payload,
-	error,
-} );
-
-export const requestUnfollowTag = ( tag ) => ( {
-	type: READER_UNFOLLOW_TAG_REQUEST,
-	payload: {
-		tag,
-		slug: slugify( tag ),
-	},
-} );
-
-export const receiveUnfollowTag = ( { payload, error } ) => ( {
-	type: READER_UNFOLLOW_TAG_RECEIVE,
-	payload,
-	error,
-} );
 
 export const requestFollowTag = ( tag ) => ( {
 	type: READER_FOLLOW_TAG_REQUEST,
