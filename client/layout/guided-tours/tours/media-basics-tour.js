3ea9b67c63dcf8aa50226b6c948ac516ad764672
JerrySarcastic <fittingsites@gmail.com>
2017-03-23 16:43:38 +0800

    Add continue action to step 2 and create step 3

--- client/layout/guided-tours/tours/media-basics-tour.js
+++ client/layout/guided-tours/tours/media-basics-tour.js
@@ -18,6 +18,7 @@ import {
     ButtonRow,
     Next,
     Quit,
+    Continue
 } from 'layout/guided-tours/config-elements';
 import {
     isNewUser,
@@ -59,11 +60,21 @@ export const MediaBasicsTour = makeTour(
             <img src="https://cldup.com/AnA1V5AnoE.gif"
             style={ { marginBottom: '10px', border: '3px solid #00AADC', borderRadius: '4px' } }
             />
-            <ButtonRow>
-                <Next step="select-image">Continue</Next>
-                <Quit>Quit</Quit>
-            </ButtonRow>
+            <Continue click step="choose-image" target=".media-library__list-item-figure">
+                Next choose an image from the media library to continue.
+            </Continue>
         </Step>
-
+        
+        <Step name="choose-image"
+            placement="below"
+            arrow="top-left"
+            //when={ and( isImageSelected ) }
+            target=".editor-media-modal__secondary-action"
+            >
+            <Continue click step="launch-modal" target=".editor-media-modal__secondary-action">
+                Now that an item is selected you can click the Edit button to continue.
+            </Continue>
+        </Step>
+        
     </Tour>
 );
