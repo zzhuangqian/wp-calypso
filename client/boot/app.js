c8f8b7307f74b85f01b351104cc5d97e566200d2
Yury Michurin <yury@automattic.com>
2017-04-20 11:49:05 +0300

    Remove es6 function usage warning

--- client/boot/app.js
+++ client/boot/app.js
@@ -1,8 +1,5 @@
 // Initialize localStorage polyfill before any dependencies are loaded
 require( 'lib/local-storage' )();
-if ( process.env.NODE_ENV === 'development' ) {
-	require( 'lib/wrap-es6-functions' )();
-}
 
 /**
  * External dependencies
