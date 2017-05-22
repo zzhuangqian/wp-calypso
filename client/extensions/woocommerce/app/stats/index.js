0d4fb524d8525194779985a6ca6995d9c0faea64
Paul Sealock <psealock@gmail.com>
2017-05-22 14:23:41 +1200

    Woo Stats: Add Main Chart
    reconnect stats nav
    
    pass query to stats chart
    
    make shared parseChartData function
    
    remove old definition
    
    use data.orders
    
    remove data print
    
    parseChartData input payload
    
    pass query to stats chart
    
    make shared parseChartData function
    
    remove old definition
    
    use data.orders
    
    remove data print
    
    Woo Stats - Add chart element
    
    buildChartData as a function of tab
    
    parseChartData input payload
    
    pass in selected date
    
    enable select date
    
    calculate net sales
    
    improve legend API

--- client/extensions/woocommerce/app/stats/index.js
+++ client/extensions/woocommerce/app/stats/index.js
@@ -1,7 +1,7 @@
 /**
  * External dependencies
  */
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { localize } from 'i18n-calypso';
 
@@ -15,20 +15,34 @@ import StatsChart from './stats-chart';
 
 class Stats extends Component {
 	render() {
-		const { siteId, unit } = this.props;
-		const chartQuery = {
+		const { siteId, unit, startDate, path } = this.props;
+		const today = this.props.moment().format( 'YYYY-MM-DD' );
+		const selectedDate = startDate || today;
+		const ordersQuery = {
 			unit,
-			date: this.props.moment().format( 'YYYY-MM-DD' ),
+			date: today,
 			quantity: '30'
 		};
 		return (
 			<Main className="woocommerce stats" wideLayout={ true }>
 				<StatsNavigation unit={ unit } type="orders" />
-				<StatsChart siteId={ siteId } query={ chartQuery } />
+				<StatsChart
+					siteId={ siteId }
+					query={ ordersQuery }
+					selectedDate={ selectedDate }
+					path={ path }
+				/>
 			</Main>
 		);
 	}
 }
+
+Stats.propTypes = {
+	siteId: PropTypes.number,
+	unit: PropTypes.string.isRequired,
+	startDate: PropTypes.string,
+	path: PropTypes.string.isRequired,
+};
 
 const localizedStats = localize( Stats );
 
--- client/extensions/woocommerce/app/stats/stats-chart/index.js
+++ client/extensions/woocommerce/app/stats/stats-chart/index.js
@@ -1,25 +1,96 @@
 /**
  * External dependencies
  */
-import React from 'react';
+import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
+import classnames from 'classnames';
+import page from 'page';
 
 /**
  * Internal dependencies
  */
+import Card from 'components/card';
 import QuerySiteStats from 'components/data/query-site-stats';
 import { getSiteStatsNormalizedData, isRequestingSiteStatsForQuery } from 'state/stats/lists/selectors';
+import ElementChart from 'components/chart';
+import Legend from 'components/chart/legend';
 
-const StatsChart = ( { siteId, query } ) => {
-	return (
-		<div>
-			{ siteId && <QuerySiteStats
-				siteId={ siteId }
-				statType="statsOrders"
-				query={ query }
-			/> }
-		</div>
-	);
+class StatsChart extends Component {
+	state = {
+		selectedTabIndex: 0
+	}
+
+	barClick = bar => {
+		page.redirect( `${ this.props.path }?startDate=${ bar.data.period }` );
+	}
+
+	buildChartData = ( item, selectedTab ) => {
+		const { selectedDate } = this.props;
+		const className = classnames( item.classNames.join( ' ' ), {
+			'is-selected': item.period === selectedDate,
+		} );
+		let value;
+		switch ( selectedTab.attr ) {
+			case 'total_sales':
+				value = item.total_sales;
+				break;
+			case 'net_sales':
+				value = Number( item.total_sales ) - Number( item.total_tax );
+				break;
+			case 'orders':
+				value = item.orders;
+				break;
+			case 'order_average':
+				value = item.orders === '0' ? 0 : Number( item.products ) / Number( item.orders );
+				break;
+			default:
+				value = 0;
+		}
+		return {
+			label: item.labelDay,
+			value,
+			nestedValue: null,
+			data: item,
+			tooltipData: [],
+			className,
+		};
+	}
+
+	render() {
+		const { siteId, query, data } = this.props;
+		const { selectedTabIndex } = this.state;
+		const tabs = [
+			{ label: 'Gross Sales', attr: 'total_sales' },
+			{ label: 'Net Sales', attr: 'net_sales' },
+			{ label: 'Orders', attr: 'orders' },
+			{ label: 'Order Average', attr: 'order_average' },
+		];
+		const selectedTab = tabs[ selectedTabIndex ];
+		const isLoading = ! ! data.length;
+		const chartData = data.map( item => this.buildChartData( item, selectedTab ) );
+		return (
+			<Card className="stats-chart stats-module">
+				{ siteId && <QuerySiteStats
+					siteId={ siteId }
+					statType="statsOrders"
+					query={ query }
+				/> }
+				<Legend
+					activeTab={ selectedTab }
+				/>
+				<ElementChart loading={ isLoading } data={ chartData } barClick={ this.barClick } />
+			</Card>
+		);
+	}
+}
+
+StatsChart.propTypes = {
+	data: PropTypes.array.isRequired,
+	isRequesting: PropTypes.bool.isRequired,
+	siteId: PropTypes.number,
+	query: PropTypes.object.isRequired,
+	selectedDate: PropTypes.string.isRequired,
+	path: PropTypes.string.isRequired,
 };
 
 export default connect(
