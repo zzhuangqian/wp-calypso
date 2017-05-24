3d4512e7fdab5029d502fca80b587bb688362cd6
Ben Lowery <ben.lowery@automattic.com>
2017-05-24 13:08:58 -0400

    fix up busted rebase

--- client/reader/search-stream/empty.jsx
+++ client/reader/search-stream/empty.jsx
@@ -21,49 +21,49 @@ class SearchEmptyContent extends React.Component {
 	}
 
 	recordAction = () => {
-		recordAction('clicked_following_on_empty');
-		recordGaEvent('Clicked Following on EmptyContent');
-		recordTrack('calypso_reader_following_on_empty_search_stream_clicked');
+		recordAction( 'clicked_following_on_empty' );
+		recordGaEvent( 'Clicked Following on EmptyContent' );
+		recordTrack( 'calypso_reader_following_on_empty_search_stream_clicked' );
 	};
 
 	recordSecondaryAction = () => {
-		recordAction('clicked_discover_on_empty');
-		recordGaEvent('Clicked Discover on EmptyContent');
-		recordTrack('calypso_reader_discover_on_empty_search_stream_clicked');
+		recordAction( 'clicked_discover_on_empty' );
+		recordGaEvent( 'Clicked Discover on EmptyContent' );
+		recordTrack( 'calypso_reader_discover_on_empty_search_stream_clicked' );
 	};
 
 	render() {
 		const action = (
-			<a className="empty-content__action button is-primary" onClick={this.recordAction} href="/">
-				{this.props.translate('Back to Following')}
+			<a className="empty-content__action button is-primary" onClick={ this.recordAction } href="/">
+				{ this.props.translate( 'Back to Following' ) }
 			</a>
 		);
 
 		const secondaryAction = isDiscoverEnabled()
 			? <a
 					className="empty-content__action button"
-					onClick={this.recordSecondaryAction}
+					onClick={ this.recordSecondaryAction }
 					href="/discover"
 				>
-					{this.props.translate('Explore Discover')}
+					{ this.props.translate( 'Explore Discover' ) }
 				</a>
 			: null;
 
-		const message = this.props.translate('No posts found for {{query /}} for your language.', {
-			components: { query: <em>{this.props.query}</em> },
-		});
+		const message = this.props.translate( 'No posts found for {{query /}} for your language.', {
+			components: { query: <em>{ this.props.query }</em> },
+		} );
 
 		return (
 			<EmptyContent
-				title={this.props.translate('No Results')}
-				line={message}
-				action={action}
-				secondaryAction={secondaryAction}
-				illustration={'/calypso/images/drake/drake-empty-results.svg'}
-				illustrationWidth={500}
+				title={ this.props.translate( 'No Results' ) }
+				line={ message }
+				action={ action }
+				secondaryAction={ secondaryAction }
+				illustration={ '/calypso/images/drake/drake-empty-results.svg' }
+				illustrationWidth={ 500 }
 			/>
 		);
 	}
 }
 
-export default localize(SearchEmptyContent);
+export default localize( SearchEmptyContent );
--- client/reader/search-stream/index.jsx
+++ client/reader/search-stream/index.jsx
@@ -11,11 +11,11 @@ import { connect } from 'react-redux';
  */
 import ControlItem from 'components/segmented-control/item';
 import SegmentedControl from 'components/segmented-control';
-
 import CompactCard from 'components/card/compact';
 import DocumentHead from 'components/data/document-head';
 import Stream from 'reader/stream';
 import EmptyContent from './empty';
+import HeaderBack from 'reader/header-back';
 import SearchInput from 'components/search';
 import { recordAction, recordTrack } from 'reader/stats';
 import SuggestionProvider from './suggestion-provider';
@@ -32,22 +32,22 @@ class SearchStream extends Component {
 		query: React.PropTypes.string,
 	};
 
-	componentWillReceiveProps(nextProps) {
-		if (nextProps.query !== this.props.query) {
-			this.updateState(nextProps);
+	componentWillReceiveProps( nextProps ) {
+		if ( nextProps.query !== this.props.query ) {
+			this.updateState( nextProps );
 		}
 	}
 
-	updateState = (props = this.props) => {
+	updateState = ( props = this.props ) => {
 		const newState = {
-			title: this.getTitle(props),
+			title: this.getTitle( props ),
 		};
-		if (newState.title !== this.state.title) {
-			this.setState(newState);
+		if ( newState.title !== this.state.title ) {
+			this.setState( newState );
 		}
 	};
 
-	getTitle = (props = this.props) => {
+	getTitle = ( props = this.props ) => {
 		return props.query;
 	};
 
@@ -57,18 +57,18 @@ class SearchStream extends Component {
 
 	updateQuery = newValue => {
 		this.scrollToTop();
-		const trimmedValue = trim(newValue).substring(0, 1024);
+		const trimmedValue = trim( newValue ).substring( 0, 1024 );
 
 		if (
-			(trimmedValue !== '' && trimmedValue.length > 1 && trimmedValue !== this.props.query) ||
+			( trimmedValue !== '' && trimmedValue.length > 1 && trimmedValue !== this.props.query ) ||
 			newValue === ''
 		) {
-			this.props.onQueryChange(newValue);
+			this.props.onQueryChange( newValue );
 		}
 	};
 
 	scrollToTop = () => {
-		window.scrollTo(0, 0);
+		window.scrollTo( 0, 0 );
 	};
 
 	handleStreamMounted = ref => {
@@ -80,28 +80,28 @@ class SearchStream extends Component {
 	};
 
 	resizeSearchBox = () => {
-		if (this.searchBoxRef && this.streamRef) {
-			const width = this.streamRef.getClientRects()[0].width;
-			if (width > 0) {
-				this.searchBoxRef.style.width = `${width}px`;
+		if ( this.searchBoxRef && this.streamRef ) {
+			const width = this.streamRef.getClientRects()[ 0 ].width;
+			if ( width > 0 ) {
+				this.searchBoxRef.style.width = `${ width }px`;
 			}
 		}
 	};
 
 	componentDidMount() {
-		this.resizeListener = window.addEventListener('resize', debounce(this.resizeSearchBox, 50));
+		this.resizeListener = window.addEventListener( 'resize', debounce( this.resizeSearchBox, 50 ) );
 		this.resizeSearchBox();
 	}
 
 	componentWillUnmount() {
-		window.removeEventListener('resize', this.resizeListener);
+		window.removeEventListener( 'resize', this.resizeListener );
 	}
 
-	placeholderFactory = ({ key, ...rest }) => {
-		if (!this.props.query) {
+	placeholderFactory = ( { key, ...rest } ) => {
+		if ( ! this.props.query ) {
 			return (
-				<div className="search-stream__recommendation-list-item" key={key}>
-					<RelatedPostCard {...rest} />
+				<div className="search-stream__recommendation-list-item" key={ key }>
+					<RelatedPostCard { ...rest } />
 				</div>
 			);
 		}
@@ -109,157 +109,160 @@ class SearchStream extends Component {
 	};
 
 	useRelevanceSort = () => {
-		recordAction('search_page_clicked_relevance_sort');
-		recordTrack('calypso_reader_clicked_search_sort', {
+		recordAction( 'search_page_clicked_relevance_sort' );
+		recordTrack( 'calypso_reader_clicked_search_sort', {
 			query: this.props.query,
 			sort: 'relevance',
-		});
-		this.props.onSortChange('relevance');
+		} );
+		this.props.onSortChange( 'relevance' );
 	};
 
 	useDateSort = () => {
-		recordAction('search_page_clicked_date_sort');
-		recordTrack('calypso_reader_clicked_search_sort', {
+		recordAction( 'search_page_clicked_date_sort' );
+		recordTrack( 'calypso_reader_clicked_search_sort', {
 			query: this.props.query,
 			sort: 'date',
-		});
-		this.props.onSortChange('date');
+		} );
+		this.props.onSortChange( 'date' );
 	};
 
 	render() {
 		const { query, suggestions, translate } = this.props;
-		const emptyContent = <EmptyContent query={query} />;
+		const emptyContent = <EmptyContent query={ query } />;
 		const sortOrder = this.props.postsStore && this.props.postsStore.sortOrder;
 
-		const transformStreamItems = !query || query === ''
-			? postKey => ({ ...postKey, isRecommendation: true })
+		const transformStreamItems = ! query || query === ''
+			? postKey => ( { ...postKey, isRecommendation: true } )
 			: identity;
 
 		let searchPlaceholderText = this.props.searchPlaceholderText;
-		if (!searchPlaceholderText) {
-			searchPlaceholderText = this.props.translate('Search billions of WordPress.com posts…');
+		if ( ! searchPlaceholderText ) {
+			searchPlaceholderText = translate( 'Search billions of WordPress.com posts…' );
 		}
 
 		const suggestionList = initial(
-			flatMap(suggestions, suggestion => [
-				<Suggestion
-					suggestion={suggestion.text}
-					source="search"
-					sort={sortOrder === 'date' ? sortOrder : undefined}
-					railcar={suggestion.railcar}
-				/>,
-				', ',
-			])
+			flatMap(
+				suggestions,
+				suggestion => [
+					<Suggestion
+						suggestion={ suggestion.text }
+						source="search"
+						sort={ sortOrder === 'date' ? sortOrder : undefined }
+						railcar={ suggestion.railcar }
+					/>,
+					', ',
+				]
+			)
 		);
 
-		const documentTitle = this.props.translate('%s ‹ Reader', {
-			args: this.state.title || this.props.translate('Search'),
-		});
+		const documentTitle = translate( '%s ‹ Reader', {
+			args: this.state.title || this.props.translate( 'Search' ),
+		} );
 
-		const TEXT_RELEVANCE_SORT = this.props.translate('Relevance', {
+		const TEXT_RELEVANCE_SORT = translate( 'Relevance', {
 			comment: 'A sort order, showing the most relevant posts first.',
-		});
+		} );
 
-		const TEXT_DATE_SORT = this.props.translate('Date', {
+		const TEXT_DATE_SORT = translate( 'Date', {
 			comment: 'A sort order, showing the most recent posts first.',
-		});
+		} );
 
 		return (
 			<Stream
-				{...this.props}
-				followSource={SEARCH_RESULTS}
-				listName={translate('Search')}
-				emptyContent={emptyContent}
-				showFollowInHeader={true}
-				placeholderFactory={this.placeholderFactory}
+				{ ...this.props }
+				followSource={ SEARCH_RESULTS }
+				listName={ translate( 'Search' ) }
+				emptyContent={ emptyContent }
+				showFollowInHeader={ true }
+				placeholderFactory={ this.placeholderFactory }
 				className="search-stream"
-				shouldCombineCards={true}
-				transformStreamItems={transformStreamItems}
+				shouldCombineCards={ true }
+				transformStreamItems={ transformStreamItems }
 			>
-				{this.props.showBack && <HeaderBack />}
-				<div ref={this.handleStreamMounted} />
-				<div className="search-stream__fixed-area" ref={this.handleSearchBoxMounted}>
-					<DocumentHead title={documentTitle} />
+				{ this.props.showBack && <HeaderBack /> }
+				<div ref={ this.handleStreamMounted } />
+				<div className="search-stream__fixed-area" ref={ this.handleSearchBoxMounted }>
+					<DocumentHead title={ documentTitle } />
 					<MobileBackToSidebar>
-						<h1>{translate('Streams')}</h1>
+						<h1>{ translate( 'Streams' ) }</h1>
 					</MobileBackToSidebar>
 					<CompactCard className="search-stream__input-card">
 						<SearchInput
-							onSearch={this.updateQuery}
-							onSearchClose={this.scrollToTop}
-							autoFocus={this.props.autoFocusInput}
-							delaySearch={true}
-							delayTimeout={500}
-							placeholder={searchPlaceholderText}
-							initialValue={query || ''}
-							value={query || ''}
+							onSearch={ this.updateQuery }
+							onSearchClose={ this.scrollToTop }
+							autoFocus={ this.props.autoFocusInput }
+							delaySearch={ true }
+							delayTimeout={ 500 }
+							placeholder={ searchPlaceholderText }
+							initialValue={ query || '' }
+							value={ query || '' }
 						/>
-						{query &&
+						{ query &&
 							<SegmentedControl compact className="search-stream__sort-picker">
-								<ControlItem selected={sortOrder !== 'date'} onClick={this.useRelevanceSort}>
-									{TEXT_RELEVANCE_SORT}
+								<ControlItem selected={ sortOrder !== 'date' } onClick={ this.useRelevanceSort }>
+									{ TEXT_RELEVANCE_SORT }
 
 								</ControlItem>
-								<ControlItem selected={sortOrder === 'date'} onClick={this.useDateSort}>
-									{TEXT_DATE_SORT}
+								<ControlItem selected={ sortOrder === 'date' } onClick={ this.useDateSort }>
+									{ TEXT_DATE_SORT }
 								</ControlItem>
-							</SegmentedControl>}
+							</SegmentedControl> }
 					</CompactCard>
+
 					<p className="search-stream__blank-suggestions">
-						{suggestions &&
-							this.props.translate('Suggestions: {{suggestions /}}.', {
+						{ suggestions &&
+							this.props.translate( 'Suggestions: {{suggestions /}}.', {
 								components: {
 									suggestions: suggestionList,
 								},
-							})}&nbsp;
+							} ) }&nbsp;
 					</p>
 
 					<hr className="search-stream__fixed-area-separator" />
 				</div>
-				{query && <ConnectedTerribleSitesResults query={query} />}
+				{ query && <ConnectedSiteResults query={ query } /> }
 			</Stream>
 		);
 	}
 }
 
-class TerribleSitesResults extends React.Component {
+class SitesResults extends React.Component {
 	componentWillMount() {
-		this.props.requestFeedSearch(this.props.query);
+		this.props.requestFeedSearch( this.props.query );
 	}
 
-	componentWillReceiveProps(nextProps) {
-		if (nextProps.query !== this.props.query) {
-			this.props.requestFeedSearch(nextProps.query);
+	componentWillReceiveProps( nextProps ) {
+		if ( nextProps.query !== this.props.query ) {
+			this.props.requestFeedSearch( nextProps.query );
 		}
 	}
 
 	render() {
 		return (
 			<div className="sites-results">
-				{this.props.feeds
-					? this.props.feeds.map(feed => (
-							<div key={feed.feed_ID} className="site-result">
-								<div className="site-title">
-									<a href={`/read/feeds/${feed.feed_ID}`}>{feed.title}</a>
-								</div>
-								<div className="site-url">
-									<a href={feed.URL} rel="noopener noreferrer" target="_blank">{feed.URL}</a>
-								</div>
-							</div>
-						))
-					: 'no results'}
+				{ this.props.feeds
+					? this.props.feeds.map(
+							feed => (
+								<SubscriptionListItem
+									key={ feed.feed_ID }
+									feedId={ feed.feed_ID }
+									blogId={ feed.blogId }
+								/>
+							)
+						)
+					: 'no results' }
 			</div>
 		);
 	}
 }
 
-const ConnectedTerribleSitesResults = connect(
-	(state, ownProps) => {
+const ConnectedSiteResults = connect(
+	( state, ownProps ) => {
 		return {
-			feeds: getReaderFeedsForQuery(state, ownProps.query),
+			feeds: getReaderFeedsForQuery( state, ownProps.query ),
 		};
 	},
 	{ requestFeedSearch }
-)(TerribleSitesResults);
+)( SitesResults );
 
-export default SuggestionProvider(localize(SearchStream));
+export default SuggestionProvider( localize( SearchStream ) );
