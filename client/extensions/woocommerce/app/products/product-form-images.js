913a7693b2b24a31909bbc6d739aabf8a1c0ecb1
Justin Shreve <justin.shreve@gmail.com>
2017-05-25 13:30:14 -0700

    Moves uploading to a separate component.

--- client/extensions/woocommerce/app/products/product-form-images.js
+++ client/extensions/woocommerce/app/products/product-form-images.js
@@ -3,25 +3,16 @@
  */
 import React, { Component, PropTypes } from 'react';
 import { localize } from 'i18n-calypso';
-import Gridicon from 'gridicons';
-import { head, uniqueId } from 'lodash';
-import { connect } from 'react-redux';
+import { find } from 'lodash';
 
 /**
  * Internal dependencies
  */
-import Button from 'components/button';
-import DropZone from 'components/drop-zone';
 import FormLabel from 'components/forms/form-label';
-import FilePicker from 'components/file-picker';
 import FormSettingExplanation from 'components/forms/form-setting-explanation';
 import ImagePreloader from 'components/image-preloader';
-import MediaActions from 'lib/media/actions';
-import MediaUtils from 'lib/media/utils';
-import MediaStore from 'lib/media/store';
+import ProductImageUploader from '../../components/product-image-uploader';
 import Spinner from 'components/spinner';
-
-import { errorNotice as errorNoticeAction } from 'state/notices/actions';
 
 class ProductFormImages extends Component {
 	// TODO Shape of product
@@ -31,86 +22,57 @@ class ProductFormImages extends Component {
 		siteId: PropTypes.number,
 	};
 
-	state = {
-		isUploading: false,
-		previews: [],
-		placeholders: [],
-	};
+	constructor( props ) {
+		super( props );
 
-	onPick = ( files ) => {
-		const { product, editProduct, translate, siteId, errorNotice } = this.props;
-		const productImages = product.images && [ ...product.images ] || [];
+		const { product } = this.props;
 
-		// DropZone supplies an array, FilePicker supplies a FileList
-		const images = Array.isArray( files ) ? MediaUtils.filterItemsByMimePrefix( files, 'image' ) : [ ...files ];
-		if ( ! images ) {
-			return false;
-		}
+		// An image with an id 0 is a WooCommerce placeholder image.
+		const images = product.images && product.images.filter( i => i.id !== 0 ) || [];
 
-		const transientIds = [];
-		const filesToUpload = [];
-		const previews = [];
-		const previewByTransientId = [];
+		this.state = {
+			images,
+		};
+	}
 
-		images.forEach( function( image ) {
-			const transientId = uniqueId( 'product-images' );
-			const preview = URL.createObjectURL( image );
+	onUpload = ( file ) => {
+		const { product, editProduct } = this.props;
+		const images = product.images && [ ...product.images ] || [];
+		images.push( {
+			id: file.ID,
+			src: file.URL,
+		} );
+		editProduct( product, { images } );
 
-			transientIds.push( transientId );
-			previews.push( preview );
-			filesToUpload.push( {
-				ID: transientId,
-				fileContents: image,
-				fileName: image.name,
-			} );
-			previewByTransientId[ transientId ] = preview;
+		const stateImages = [ ...this.state.images ].map( ( i ) => {
+			if ( i.transientId === file.transientId ) {
+				return { ...i, src: file.URL, placeholder: null };
+			}
+			return i;
 		} );
 
 		this.setState( {
-			isUploading: true,
-			previews,
-			placeholders: [],
+			images: stateImages,
 		} );
+	}
 
-		const placeholders = [];
-		const handleUpload = () => {
-			const transientId = head( transientIds );
-			const media = MediaStore.get( siteId, transientId );
-			const isUploadInProgress = media && MediaUtils.isItemBeingUploaded( media );
+	onSelect = ( previews ) => {
+		const { images } = this.state;
+		const newImages = Object.entries( previews ).map( ( [ transientId, src ] ) => {
+			return { placeholder: src, transientId };
+		} );
+		this.setState( {
+			images: [ ...images, ...newImages ],
+		} );
+	}
 
-			// File has finished uploading or failed.
-			if ( ! isUploadInProgress ) {
-				transientIds.shift();
-				if ( media ) {
-					// TODO: Other fields here?
-					productImages.push( {
-						src: media.URL
-					} );
-					placeholders[ media.URL ] = previewByTransientId[ transientId ];
-					previews.shift();
-					this.setState( {
-						previews,
-						placeholders,
-					} );
-					editProduct( product, { images: productImages } );
-				} else {
-					errorNotice( translate( 'There was a problem uploading %s.', {
-						args: media && media.file || translate( 'a product image' )
-					} ) );
-				}
+	onError = ( file ) => {
+		console.log( file );
+	}
 
-				// All images have been uploaded.
-				if ( transientIds.length === 0 ) {
-					this.setState( {
-						isUploading: false,
-					} );
-					MediaStore.off( 'change', handleUpload );
-				}
-			}
-		};
-
-		MediaStore.on( 'change', handleUpload );
-		MediaActions.add( siteId, filesToUpload );
+	onFinish = ( ids ) => {
+		console.log( 'done!' );
+		console.log( ids );
 	}
 
 	removeImage = ( index ) => {
@@ -118,87 +80,27 @@ class ProductFormImages extends Component {
 		const images = product.images && [ ...product.images ] || [];
 		images.splice( index, 1 );
 		editProduct( product, { images } );
+		// TODO Remove image from state too.
 	}
 
 	render() {
-		const { product, translate } = this.props;
-		const { isUploading, previews, placeholders } = this.state;
-
-		// An image with id 0 is a placeholder image.
-		const images = product.images && product.images.filter( i => i.id !== 0 ) || [];
+		const { translate, siteId } = this.props;
+		const { images } = this.state;
 
 		return (
 			<div className="products__product-form-images-wrapper">
 
 				<FormLabel>{ translate( 'Product Images' ) }</FormLabel>
 
-				{ images.length === 0 && ! isUploading && (
-					<div className="products__product-form-images-placeholder">
-						<div>
-							<div className="products__product-form-images-picker">
-								<FilePicker multiple accept="image/*" onPick={ this.onPick }>
-									<Gridicon icon="add-outline" size={ 36 } />
-									<p>{ translate( 'Add images' ) }</p>
-								</FilePicker>
-							</div>
-							<DropZone onFilesDrop={ this.onPick } />
-						</div>
-					</div>
-				) }
+				<ProductImageUploader
+					onSelect={ this.onSelect }
+					onUpload={ this.onUpload }
+					onError={ this.onError }
+					onFinish={ this.onFinish }
+					siteId={ siteId }
+				/>
 
-				<div className="products__product-form-images">
-					{ images.map( ( image, index ) => {
-						const removeImage = () => {
-							this.removeImage( index );
-						};
-						return (
-							<div className="products__product-form-images-item" key={ index }>
-								<figure>
-									<ImagePreloader
-										src={ image.src }
-										placeholder={ ( <img src={ placeholders[ image.src ] } /> ) }
-									/>
-								</figure>
-								<Button
-									onClick={ removeImage }
-									compact
-									className="products__product-form-images-item-remove">
-									<Gridicon
-										icon="cross"
-										size={ 24 }
-										className="products__product-form-images-item-remove-icon"
-									/>
-								</Button>
-							</div>
-						);
-					} ) }
-					{ previews && previews.map( function( preview, index ) {
-						return (
-							<div className="products__product-form-images-item preview" key={ 'preview-' + index }>
-								<figure>
-									<img src={ preview } />
-									<Spinner />
-								</figure>
-							</div>
-						);
-					} ) }
-
-					{ ( images.length > 0 || previews.length > 0 ) && ! isUploading && (
-						<div className="products__product-form-images-picker compact">
-							<FilePicker multiple accept="image/*" onPick={ this.onPick } >
-								<Gridicon icon="add-outline" size={ 24 } />
-							</FilePicker>
-						</div>
-					) }
-				</div>
-
-				{ /* TODO: Component Cleanup/Breakup: Part of this can be trying to make multiple uploads / concurrent uploading work.
-					Perhaps we have a <ProductImagePicker /> component, with an onSelect prop that returns
-					a list of previews based on selected files, and an onUpload that returns when a file has finished uploading.
-					If we can keep track of these, we could keep the preview and image area separate from the uploading process
-					and also allow concurrent uploading. This would also allow us to use shared code for variations..
-					maybe have only a singleUpload prop as well for that..
-				 */ }
+				{ /* TODO: Image preview and display */ }
 				{ /* TODO: CSS Layout */ }
 				{ /* TODO: Review */ }
 				{ /* TODO: Variations will be a different control... */ }
@@ -211,4 +113,4 @@ class ProductFormImages extends Component {
 	}
 }
 
-export default connect( null, { errorNotice: errorNoticeAction } )( localize( ProductFormImages ) );
+export default localize( ProductFormImages );
---
+++ client/extensions/woocommerce/components/product-image-uploader/index.js
@@ -0,0 +1,162 @@
+/**
+ * External dependencies
+ */
+import React, { Component, PropTypes } from 'react';
+import { localize } from 'i18n-calypso';
+import Gridicon from 'gridicons';
+import { head, uniqueId } from 'lodash';
+import { connect } from 'react-redux';
+
+/**
+ * Internal dependencies
+ */
+import DropZone from 'components/drop-zone';
+import { errorNotice as errorNoticeAction } from 'state/notices/actions';
+import FilePicker from 'components/file-picker';
+import MediaActions from 'lib/media/actions';
+import MediaUtils from 'lib/media/utils';
+import MediaStore from 'lib/media/store';
+
+/* TODO: No state? Make stateless component? */
+class ProductImageUploader extends Component {
+
+	static propTypes = {
+		siteId: PropTypes.number,
+		multiple: PropTypes.bool,
+		compact: PropTypes.bool,
+		onSelect: PropTypes.func,
+		onUpload: PropTypes.func,
+		onError: PropTypes.func,
+		onFinish: PropTypes.func,
+	};
+
+	static defaultProps = {
+		multiple: true,
+		compact: false,
+	}
+
+	onPick = ( files ) => {
+		const { siteId, multiple } = this.props;
+		const { translate, errorNotice } = this.props;
+		const { onSelect, onUpload, onError, onFinish } = this.props;
+
+		// DropZone supplies an array, FilePicker supplies a FileList
+		let images = Array.isArray( files ) ? MediaUtils.filterItemsByMimePrefix( files, 'image' ) : [ ...files ];
+		if ( ! images ) {
+			return false;
+		}
+
+		if ( multiple === false ) {
+			images = [ images.shift() ];
+		}
+
+		const transientIds = [];
+		const filesToUpload = [];
+		const previews = [];
+		images.forEach( function( image ) {
+			const transientId = uniqueId( 'product-images' );
+			transientIds.push( transientId );
+			filesToUpload.push( {
+				ID: transientId,
+				fileContents: image,
+				fileName: image.name,
+			} );
+			previews[ transientId ] = URL.createObjectURL( image );
+		} );
+
+		onSelect( previews );
+
+		const uploadedIds = [];
+		const handleUpload = () => {
+			const transientId = head( transientIds );
+			const media = MediaStore.get( siteId, transientId );
+			const isUploadInProgress = media && MediaUtils.isItemBeingUploaded( media );
+
+			// File has finished uploading or failed.
+			if ( ! isUploadInProgress ) {
+				if ( media ) {
+					onUpload( {
+						ID: media.ID,
+						transientId,
+						URL: media.URL,
+						placeholder: previews[ transientId ],
+					} );
+					uploadedIds.push( transientId );
+				} else {
+					onError( {
+						file: media,
+						transientId,
+					} );
+					errorNotice( translate( 'There was a problem uploading %s.', {
+						args: media && media.file || translate( 'an image' )
+					} ) );
+				}
+
+				transientIds.shift();
+				if ( transientIds.length === 0 ) {
+					MediaStore.off( 'change', handleUpload );
+					onFinish( uploadedIds );
+				}
+			}
+		};
+
+		MediaStore.on( 'change', handleUpload );
+		MediaActions.add( siteId, filesToUpload );
+	}
+
+	renderCompactUploader() {
+		const { multiple } = this.props;
+		return (
+			<div className="product-image-uploader__picker compact">
+				<FilePicker multiple={ multiple } accept="image/*" onPick={ this.onPick } >
+					<Gridicon icon="add-outline" size={ 24 } />
+				</FilePicker>
+			</div>
+		);
+	}
+
+	renderUploader() {
+		const { translate, multiple } = this.props;
+		return (
+			<div className="product-image-uploader__wrapper">
+				<div>
+					<div className="product-image-uploader__picker">
+						<FilePicker multiple={ multiple } accept="image/*" onPick={ this.onPick }>
+							<Gridicon icon="add-outline" size={ 36 } />
+							<p>{ translate( 'Add images' ) }</p>
+						</FilePicker>
+					</div>
+					<DropZone onFilesDrop={ this.onPick } />
+				</div>
+			</div>
+		);
+	}
+
+	renderChildren() {
+		return React.Children.map( this.props.children, function( child ) {
+			return <div>{ child }</div>;
+		} );
+	}
+
+	render() {
+		const { compact, children, multiple } = this.props;
+
+		if ( children !== undefined ) {
+			return (
+				<FilePicker multiple={ multiple } accept="image/*" onPick={ this.onPick }>
+					{ this.renderChildren() }
+				</FilePicker>
+			);
+		}
+
+		const uploader = compact && this.renderCompactUploader() || this.renderUploader();
+		return (
+			<div>
+				{ uploader }
+			</div>
+		);
+	}
+
+}
+
+export default connect( null, { errorNotice: errorNoticeAction } )( localize( ProductImageUploader ) );
